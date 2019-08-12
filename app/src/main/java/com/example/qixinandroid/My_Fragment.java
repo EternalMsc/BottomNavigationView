f17ae0ab66143695f37d5f73e1000b3a1939fdb6
msc <1273590269@qq.com>
2019-08-12 20:02:18 +0800

    Initial commit

---
+++ app/src/main/java/com/example/qixinandroid/My_Fragment.java
@@ -0,0 +1,18 @@
+package com.example.qixinandroid;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class My_Fragment extends Fragment {
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view=inflater.inflate(R.layout.my_fragment,null);
+
+        return view;
+    }
+}
